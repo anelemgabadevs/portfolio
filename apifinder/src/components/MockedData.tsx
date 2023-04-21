54248d3d3a7a74c6567c66d5e07d453f2e83fe91
anelemgabadevs <115465047+anelemgabadevs@users.noreply.github.com>
2023-04-21 12:45:27 +0200

    starting api finder

---
+++ apifinder/src/components/MockedData.tsx
@@ -0,0 +1,18 @@
+import IMockService from '@/services/MockService'
+
+const mockData:IMockService[]=
+
+[       {
+
+    
+        api_title : 'Api Title: ',
+        description:'Description',
+        link:'Api Link: ',
+        category:'Category',
+        https:'https: ',
+        auth:'Authentaction: ',
+    
+        }
+]
+
+export default mockData
---
+++ apifinder/src/services/ApiDataService.ts
@@ -0,0 +1,14 @@
+
+export interface IApiDataType {
+
+    API:string;
+    Description:string;
+    Auth:string;
+    HTTPS:string;
+    Cors:string;
+    Link:string;
+    Category:string;
+
+}
+
+export default IApiDataType
